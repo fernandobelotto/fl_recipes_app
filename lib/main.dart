3f565e451185bcefd353377b4061a74e633ffc87
fernandobosco <fbosco@dlocal.com>
2021-12-25 22:57:43 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+import 'recipe_detail.dart';
+
+void main() {
+  runApp(const RecipesApp());
+}
+
+class RecipesApp extends StatelessWidget {
+  const RecipesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme
+              .copyWith(primary: Colors.grey, secondary: Colors.black)),
+      home: const MyHomePage(title: 'Recipe Calculador'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+        elevation: 2.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage(recipe.imageUrl)),
+              const SizedBox(
+                height: 14.0,
+              ),
+              Text(
+                recipe.label,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino',
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SafeArea(
+            child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+                child: buildRecipeCard(Recipe.samples[index]),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return RecipeDetail(recipe: Recipe.samples[index]);
+                  }));
+                });
+          },
+        )));
+  }
+}
